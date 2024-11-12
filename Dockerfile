fa044529971b7970cd77adda6690cf389ddf3f96
Umidjon Mukhtorov <mukhtorov.me@gmail.com>
2024-11-12 11:42:09 +0500

    add env variable substitution

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,11 @@
 FROM devopsfaith/krakend:2.7
 
-COPY config/krakend.json /etc/krakend/krakend.json
+COPY config/krakend.json /etc/krakend/krakend-config.json
 
 COPY plugins /opt/krakend/plugins/
 
 COPY script/entrypoint.sh /
 
-RUN chmod +x /entrypoint.sh
+RUN chmod +x /entrypoint.sh && apk add envsubst
+
+CMD ["/entrypoint.sh"]
--- script/entrypoint.sh
+++ script/entrypoint.sh
@@ -1,21 +1,5 @@
 #!/bin/sh
 
-# Check if the krakend.json file exists
-JSON_FILE="/etc/krakend/krakend.json"
+envsubst < /etc/krakend/krakend-config.json > /etc/krakend/krakend.json
 
-if [ ! -f "$JSON_FILE" ]; then
-    echo "File $JSON_FILE does not exist."
-    exit 1
-fi
-
-printenv | while IFS='=' read -r v value; do
-    if [ -n "$v" ]; then
-        # echo "${v}=${value}"
-        
-        # Escape special characters in the value for sed
-        ev=$(printf '%s\n' "$value" | sed 's/[\/&]/\\&/g')
-
-        # Replace placeholders in the krakend.json file
-        sed -i "s|${v}|${ev}|g" "$JSON_FILE"
-    fi
-done
+/usr/bin/krakend run -d -c /etc/krakend/krakend.json -p 80
